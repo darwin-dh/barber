f721f0837a3e3070c52b5d5f71c5224926106854
darwin-dh <darwinquishpe93@gmail.com>
2022-03-25 18:40:46 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* $(document).ready(function () {
+    $('nav a').each(function (index) {
+        $(this).css({
+            'top': '-200px'
+        });
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500));
+    });
+
+});
+ */
